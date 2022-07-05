9f1ee9122aaf173bafadeba15ea739cb25fbd3b8
Tim Paine <t.paine154@gmail.com>
2022-07-05 13:04:49 -0400

    fix layout save/restore, upgrade libraries

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -1,44 +1,35 @@
 import React, {useEffect, useRef} from "react";
 
 function Workspace(props) {
-  const {tables, layout} = props;
-  const {layouts, changeLayouts} = props;
+  const {tables} = props;
+  const {layout, layouts} = props;
   const workspace = useRef(null);
+
+  // restore layout when it changes
+  useEffect(() => {
+    if (layout && layouts && workspace.current) {
+      workspace.current.restore(layouts[layout]);
+    }
+  }, [layout]);
+
+  // hide the progress bar
+  useEffect(async () => {
+    if (workspace.current) {
+      const progress = document.getElementById("progress");
+      progress.setAttribute("style", "display:none;");
+    }
+  }, [workspace]);
 
   useEffect(() => {
     if (tables && workspace.current) {
-      // set layout
-      (async () => {
-        await workspace.current.restore(layout);
-        await workspace.current.flush();
-
-        // hide the progress bar
-        const progress = document.getElementById("progress");
-        progress.setAttribute("style", "display:none;");
-      })();
-
       // load tables into perspective workspace
       Object.keys(tables).forEach((key) => {
         workspace.current.addTable(key, tables[key]);
       });
-
-      // add a listener to update layouts on change
-      workspace.current.addEventListener("workspace-layout-update", async () => {
-        const modifiedConfig = await workspace.current.save();
-
-        // eslint-disable-next-line no-console
-        console.debug("Saving to localStorage:", modifiedConfig);
-        window.localStorage.setItem("polygon_io_perspective_workspace_config", JSON.stringify(modifiedConfig));
-
-        // update layouts state overriding the custom one
-        if (layouts.length > 0) {
-          changeLayouts({...layouts, "Custom Layout": modifiedConfig});
-        }
-      });
     }
   }, [tables, workspace]);
 
-  return <perspective-workspace ref={workspace} />;
+  return <perspective-workspace id="workspace" ref={workspace} />;
 }
 
 export default Workspace;
